39b8923e3d6cfd407f0b5b34655f3ae42dd2f198
Gustavo Mascarenhas <gustavo_santos2548@hotmail.com>
2026-06-24 14:38:23 -0300

    Feature Deletar

---
+++ IO/deletarCaracteres.py
@@ -0,0 +1,26 @@
+from os import path
+
+def deletar_caracteres(caminho, separador, campoAlterar, numAtl, id_row):
+        novas_linhas = []
+
+        with open(caminho, "r") as f:
+            for rows in f:
+                dados = rows.lstrip("|").strip().split(separador)
+
+                if dados[0] == id_row:
+                    if campoAlterar < len(dados):
+                        tmp = dados[campoAlterar][numAtl:] 
+                        dados[campoAlterar]= tmp
+                        nova_linha = "|" + separador.join(dados)
+                        novas_linhas.append(nova_linha)
+                else:
+                    novas_linhas.append(rows.strip())
+
+                    
+        caminho_saida = path.abspath("ArquivoFormatado.txt")
+
+        with open(caminho_saida, "w") as f:
+            f.write("\n".join(novas_linhas))
+
+        return caminho_saida
+
--- IO/formateTxT.py
+++ IO/formateTxT.py
@@ -1,5 +1,6 @@
 from tkinter import messagebox
 from .abrirArquivo import processar_arquivos
+from .deletarCaracteres import deletar_caracteres
 
 class FormateTxt:
         def __init__(self, app):
@@ -17,6 +18,32 @@ class FormateTxt:
                     self.app.separador_var.get(),
                     int(self.app.campo_var.get()),
                     self.app.num_var.get(),
+                    self.app.id_var.get()
+                )
+
+                self.app.process_status.config(text="Processado com sucesso")
+
+                messagebox.showinfo(
+                    "Concluído",
+                    f"Arquivo gerado com sucesso.\n\nLocal:\n{caminho_saida}"
+                )
+
+            except Exception as e:
+                self.app.process_status.config(text=f"Erro: Algum campo não foi preenchido corretamente! \n{str(e)}")
+                
+             
+        def executar_delete(self):
+  
+            if not self.app.caminho_arquivo:
+                self.app.process_status.config(text="Nenhum arquivo carregado")
+                return
+
+            try:
+                caminho_saida = deletar_caracteres(
+                    self.app.caminho_arquivo,
+                    self.app.separador_var.get(),
+                    int(self.app.campo_var.get()),
+                    int(self.app.num_var.get()),
                     self.app.id_var.get()
                 )
 
